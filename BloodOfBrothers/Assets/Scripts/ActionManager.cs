5d4db7c0d976fb7563f886826c0555471f0333da
David Richman <65867289+BabyDrakeZ@users.noreply.github.com>
2023-04-21 01:31:30 -0400

    (Don't pull this in the morning)

--- BloodOfBrothers/Assets/Scripts/ActionManager.cs
+++ BloodOfBrothers/Assets/Scripts/ActionManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class ActionManager : MonoBehaviour
 {
     // Collects the current selection   
-    private HashSet<GameObject> selection = new HashSet<GameObject>();
+    private HashSet<Unit> selection = new HashSet<Unit>();
 
     // Stores the current Coroutine controlling he selection process
     private Coroutine selectionRoutine;
@@ -30,8 +30,54 @@ public class ActionManager : MonoBehaviour
         {
             EndSelection();
         }
+        if (selection.Count > 0)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                executeMove();
+                Debug.Log("MoveExecuted");
+            }
+            if (Input.GetKey(KeyCode.LeftControl) && Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                executeRotate();
+                Debug.Log("RotationExecuted");
+            }
+        }
+        
     }
-    
+    private void executeRotate()
+    {
+        foreach (Unit unit in selection)
+        {
+            float rotation = Mathf.Clamp(Input.GetAxis("Mouse ScrollWheel"), -1, 1) * Time.deltaTime * unit.rotationSpeed * 1000;
+            unit.transform.Rotate(Vector3.up, rotation);
+        }
+    }
+    private void executeMove()
+    {
+        foreach (Unit unit in selection)
+        {
+            var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                GameObject obj = hit.collider.gameObject;
+                if (obj.tag == "Terrain")
+                {
+                    unit.destination = hit.point;
+                }
+                if (obj.tag == "Unit")
+                {
+                    Unit enemy = obj.GetComponentInParent<Unit>();
+                    if (enemy != null && !selection.Contains(enemy) && enemy.team != 1)
+                    {
+                        unit.target_stats = enemy;
+                        unit.target = enemy.gameObject;
+                    }
+                }
+            }
+        }
+    }
+
     public void StartSelection()
     {
         // if there is already a selection running you don't wanr to start another one
@@ -43,8 +89,8 @@ public class ActionManager : MonoBehaviour
     
     public void EndSelection()
     {
+
         if(selectionRoutine == null) return;
-    
         StopCoroutine (selectionRoutine);
         Debug.Log("Ended");
         selectionRoutine = null;
@@ -58,13 +104,23 @@ public class ActionManager : MonoBehaviour
         // This is ok in a Coroutine as long as you yield somewhere within it
         while(true)
         {
-            var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
- 
-            if(Physics.Raycast(ray, out var hit))
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray, 200);
+            for (int i = 0; i < hits.Length; i++)
             {
-                if(!selection.Contains(hit.collider.gameObject)) {
-                   selection.Add(hit.collider.gameObject);
-                   Debug.Log(selection.Count);
+                RaycastHit hit = hits[i];
+                {
+                    GameObject obj = hit.collider.gameObject;
+                    if (obj.tag == "Unit")
+                    {
+                        Unit stats = obj.GetComponentInParent<Unit>();
+                        if (stats != null && !selection.Contains(stats) && stats.team == 1)
+                        {
+                                selection.Add(stats);
+                                Debug.Log(selection.Count);
+                        }
+                    }
+
                 }
             }
     
---
+++ BloodOfBrothers/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float minHeight = 10;
+    public float maxHeight = 200;
+    public float ySpeed = 200;
+    public float xSpeed = 20;
+    public float borderRatio = 0.3f;
+    public bool mouseLock = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0 && this.transform.position.y > minHeight)
+            {
+                this.transform.position += Time.deltaTime * ySpeed * Vector3.down;
+            }
+            if (Input.GetAxis("Mouse ScrollWheel") < 0 && this.transform.position.y < maxHeight)
+            {
+                this.transform.position += Time.deltaTime * ySpeed * Vector3.up;
+            }
+        }
+        Vector3 mousePos = Input.mousePosition;
+        float borderY = borderRatio * Screen.height;
+        float borderX = borderRatio * Screen.width;
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            mouseLock = !mouseLock;
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            mouseLock = true;
+        }
+        if (mouseLock)
+        {
+            if (mousePos.y > Screen.height - borderY)
+            {
+                this.transform.position += Time.deltaTime * xSpeed * this.transform.up;
+            }
+            if (mousePos.y < borderY)
+            {
+                this.transform.position -= Time.deltaTime * xSpeed * this.transform.up;
+            }
+            if (mousePos.x > Screen.width - borderX)
+            {
+                this.transform.position += Time.deltaTime * xSpeed * this.transform.right;
+            }
+            if (mousePos.x < borderX)
+            {
+                this.transform.position -= Time.deltaTime * xSpeed * this.transform.right;
+            }
+        }
+    }
+}
